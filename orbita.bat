235628e522b8529c59222a97e02d83ceda375aa4
aman <amanibraev95@gmail.com>
2025-08-25 11:07:46 +0600

    initial commit

---
+++ orbita.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d %~dp0
+
+REM Запускаем продакшн сервер для уже собранного проекта
+npm start
+
+pausenpm
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,166 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import * as XLSX from "xlsx";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+
+function formatYMDshort(date: Date) {
+  const year = date.getFullYear().toString().slice(-2);
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+}
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  type RowType = [number, string, string, string, number]; // пример: 5 колонок
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const [files, setFile] = useState<RowType[]>([]);
+
+  const [startDate, setStartDate] = useState<string>("");
+  const [endDate, setEndDate] = useState<string>("");
+  const [filtered, setFiltered] = useState<RowType[]>([]);
+
+  const defaultFilter = "guest"; // сразу фильтруем по "guest"
+
+  const handleFilter = () => {
+    if (!startDate || !endDate) return;
+
+    const start = formatYMDshort(new Date(startDate));
+    const end = formatYMDshort(new Date(endDate));
+
+    const res: RowType[] = files.filter((row) => {
+      const rowDate = row[3];
+      return rowDate >= start && rowDate <= end;
+    });
+
+    setFiltered(res);
+  };
+
+  const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStartDate("");
+    setEndDate("");
+    const file = e.target.files?.[0];
+    if (!file) return; // <-- проверяем, что файл выбран
+
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      const binaryStr = event.target?.result;
+      const workbook = XLSX.read(binaryStr, { type: "binary" });
+
+      // Берём первый лист
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+      const jsonData: (string | number)[][] = XLSX.utils.sheet_to_json(
+        worksheet,
+        {
+          header: 1,
+        }
+      );
+      const header = jsonData[3]; // сохраняем заголовок
+      const dataRows = jsonData.slice(1); // остальные строки
+      const seen = new Set<string>();
+
+      const filtered = dataRows.filter((row, index) => {
+        if (index === 0) return true; // оставляем заголовок
+
+        const matchesFilter = row.some((cell) =>
+          String(cell || "")
+            .toLowerCase()
+            .includes(defaultFilter)
+        ) as unknown as RowType[];
+
+        if (!matchesFilter) return false;
+
+        const date = String(row[3]);
+
+        if (seen.has(date)) return false;
+
+        seen.add(date);
+        return true;
+      });
+
+      const finalData: RowType[] = [header, ...filtered] as RowType[];
+
+      setFile(finalData);
+    };
+
+    reader.readAsArrayBuffer(file);
+  };
+
+  return (
+    <div className="container mx-auto">
+      <main className="flex flex-col justify-around pt-10">
+        <div className="flex w-full items-center gap-2">
+          <div className="flex flex-col flex-1">
+            <div className=" flex flex-col flex-1 items-start gap-3">
+              <Label htmlFor="file">Выберите excell файл</Label>
+              <Input
+                id="file"
+                onChange={handleFileUpload}
+                type="file"
+                placeholder="Открыть"
+              />
+            </div>
+
+            <div className="flex gap-5 mt-5 items-end">
+              <div className="flex flex-col flex-1 items-start gap-3">
+                <Label>Начало</Label>
+                <Input
+                  disabled={files.length === 0}
+                  type="date"
+                  value={startDate}
+                  onChange={(e) => setStartDate(e.target.value)}
+                  className="border p-2"
+                  placeholder="начало даты"
+                  required
+                />
+              </div>
+              <div className="flex flex-col flex-1 items-start gap-3">
+                <Label>Конец</Label>
+                <Input
+                  type="date"
+                  disabled={files.length === 0}
+                  value={endDate}
+                  onChange={(e) => setEndDate(e.target.value)}
+                  className="border p-2"
+                  placeholder="конец даты"
+                  required
+                />
+              </div>
+
+              <Button
+                disabled={!startDate || !endDate}
+                onClick={handleFilter}
+                className="bg-blue-500 text-white px-4 py-2 rounded"
+              >
+                Фильтровать по периоду
+              </Button>
+            </div>
+          </div>
         </div>
+        <table className="min-w-full border border-gray-300 mt-5">
+          <tbody>
+            {filtered.map((row, i) => (
+              <tr
+                key={i}
+                className={i % 2 === 0 ? "bg-white" : "bg-gray-50"} // полосатые строки
+              >
+                {Object.values(row).map((val, j) => (
+                  <td
+                    key={j}
+                    className="px-4 py-2 border-b border-gray-300 text-sm text-gray-800"
+                  >
+                    {val as string}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
